93fd80640231b784bb45705e4bd6f7388564b5de
Cristian Le <git@lecris.dev>
2025-05-02 15:56:13 +0200

    Switch to using lief
    patchelf is failing on recent Fedora releases
    
    Signed-off-by: Cristian Le <git@lecris.dev>

--- tests/conftest.py
+++ tests/conftest.py
@@ -58,8 +58,7 @@ def pep518_wheelhouse(tmp_path_factory: pytest.TempPathFactory) -> Path:
         "wheel",
     ]
     if platform.system() == "Linux":
-        packages.append("auditwheel")
-        packages.append("patchelf")
+        packages.append("lief")
     if platform.system() == "Darwin":
         packages.append("delocate")
 
